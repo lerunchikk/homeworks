177e4394af526230a952073e7ed840cb52f88b01
lerunchik <antonovale2007@gmail.com>
2026-08-12 12:22:08 +0300

    Добавлена домашняя работа за 3 урок

---
+++ Антонова_Валерия_day3/ex_2.py
@@ -0,0 +1,6 @@
+city = input ("Введите свой любимый город: ")
+word_1 = city[0:3]
+word_2 = city[-3:]
+word_3 = word_1 + "-" + word_2
+print(f"Результат : {3*  (word_3 + " ")}")
+
---
+++ Антонова_Валерия_day3/ex_3.py
@@ -0,0 +1,9 @@
+hero = {
+    "name":"Gandalf",
+    "level": 80,
+    "inventory": ["staff", "robe", "potion"]
+}
+hero["inventory"].append("sword")
+hero["level"] +=1
+hero["guild"] = "Wizards"
+print(hero)
